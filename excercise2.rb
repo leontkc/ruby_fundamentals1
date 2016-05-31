8f51e667260643209c9d946c2be9865e7044c204
Leon Chiu <leontkchiu@gmail.com>
2016-05-31 13:48:22 -0400

    excercise 2

---
+++ excercise2.rb
@@ -0,0 +1,10 @@
+#1. good tip
+puts 55 * 0.15
+#2. string and integer
+string = "this is a string"
+number = 3
+puts "#{string} + #{number}"
+#3. output
+puts "the product of 45628 and  7839 is #{45628 * 7839}"
+#4. value of expression
+puts true
