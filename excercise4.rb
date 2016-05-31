f40efda58561623b4306307099c2a17cfbaba885
Leon Chiu <leontkchiu@gmail.com>
2016-05-31 14:06:50 -0400

    revised ex 4

--- excercise4.rb
+++ excercise4.rb
@@ -1,11 +1,9 @@
 100.times do |x|
   if x % (3*5) ==0
     puts "BitMaker"
-  elsif
-    x % 5 ==0
+  elsifx % 5 ==0
     puts "Maker"
-  elsif
-    x % 3 ==0
+  elsif  x % 3 ==0
     puts "Bit"
   else
     puts x
